b8de8851ac13ed25228df7fc04127f4960e2f662
Lorreny <38407983+lorreny@users.noreply.github.com>
2024-06-01 09:29:15 -0300

    Create exercicios-3.js

---
+++ modulo-3/exercicios-3.js
@@ -0,0 +1,10 @@
+// 3 - Crie um programa que  recebe como entrada um número e imprime todos os valores de 0 até esse valor.
+
+let number = 7
+
+while (number >= 0){
+    console.log(number)
+
+    number -= 1
+
+}
