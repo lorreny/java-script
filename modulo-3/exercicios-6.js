ba75f1074176e761a60b70bd155b5bad270976f7
Lorreny <38407983+lorreny@users.noreply.github.com>
2024-06-01 10:26:12 -0300

    Update exercicios-6.js

--- modulo-3/exercicios-6.js
+++ modulo-3/exercicios-6.js
@@ -1,2 +1,16 @@
 // 6 - Crie um programa que recebe como entrada uma lista e imprime o menor valor da lista com a seguinte mensagem: "O menor valor dessa lista é..."
 //let menorLista = [0, 16, -22, 42, 55, 23, 12]
+// verificar cada valor da lista
+//saber a extensão da lista
+//loop para percorrer
+let menorLista = [0, 16, -22, 42, 55, 23, 12]
+let contador = 0
+let menor = null
+while(contador < menorLista.length){
+    if (menorLista[contador] < menor || menor === null){
+        menor = menorLista[contador]
+
+    }
+    contador += 1
+}
+console.log(menor)
