0899e82d7004eacf96f9d9281346a22a6c75f767
Lorreny <38407983+lorreny@users.noreply.github.com>
2024-06-01 09:28:58 -0300

    Create exercicios-2.js

---
+++ modulo-3/exercicios-2.js
@@ -0,0 +1,10 @@
+// 2-  Crie um programa que dado um número (ex: 7), imprime todos os valores entre esse número e 0; 
+
+let number = 7
+
+while (number > 1){
+    console.log(number - 1)
+
+    number -= 1
+
+}
