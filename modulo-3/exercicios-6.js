0da0f7f2e608662ba67e4a5f209be40231e8da0e
Lorreny <38407983+lorreny@users.noreply.github.com>
2024-06-01 10:16:52 -0300

    Create exercicios-6.js

---
+++ modulo-3/exercicios-6.js
@@ -0,0 +1,2 @@
+// 6 - Crie um programa que recebe como entrada uma lista e imprime o menor valor da lista com a seguinte mensagem: "O menor valor dessa lista é..."
+//let menorLista = [0, 16, -22, 42, 55, 23, 12]
