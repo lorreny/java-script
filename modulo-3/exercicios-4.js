7f9216db38e74228c68a5a2bf207126760cb0541
Lorreny <38407983+lorreny@users.noreply.github.com>
2024-06-01 09:50:05 -0300

    Create exercicios-4.js

---
+++ modulo-3/exercicios-4.js
@@ -0,0 +1,13 @@
+// 4 - Crie um programa que recebe como entrada uma palavra e imprime separadamente cada letra da palavra.
+
+let letra = 'asa'
+let n1 = 0
+//console.log(letra.length)
+//console.log(letra[0])
+//console.log(letra[1])
+//console.log(letra[2])
+
+while(letra.length > n1 ){
+    console.log(letra[n1])
+    n1 += 1
+}
