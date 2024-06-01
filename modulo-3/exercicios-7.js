b94f162b81b26a2b18d2776659c2aaed46eb1b68
Lorreny <38407983+lorreny@users.noreply.github.com>
2024-06-01 10:17:13 -0300

    Create exercicios-7.js

---
+++ modulo-3/exercicios-7.js
@@ -0,0 +1,2 @@
+// 7 - Crie um programa que recebe como entrada uma lista e imprime o maior valor da lista com a seguinte mensagem: "O maior valor da lista é..." 
+//let maiorLista = [12, -33, 0, 2, 12, 21, 42]
