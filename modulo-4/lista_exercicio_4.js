b35783babb01c8bb3b0f62b748b9632cc08c8e24
Lorreny <38407983+lorreny@users.noreply.github.com>
2024-06-08 08:52:55 -0300

    Create lista_exercicio_4.js

---
+++ modulo-4/lista_exercicio_4.js
@@ -0,0 +1,9 @@
+// 4 - Observe as seguintes listas: 
+let nomes = ['Bruna', 'Gabriel', 'Marcos', 'Jéssica', 'Fernando']
+let sobrenomes = ['Oliveira', 'Santos', 'Marques', 'Castro', 'Silva']
+
+//Crie um algirtmo que combine o valor do nome com o sobrenome formando o nome completo.
+// O programa deve iterar sobre as listas fornecidas e juntar os valores correspondentes.
+for (let i = 0; i < nomes.length; i++){
+    console.log(nomes[i], sobrenomes[i])
+}
