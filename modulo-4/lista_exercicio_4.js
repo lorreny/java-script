90d7e0016c9817f94c3180a5751c38b9232541f0
Lorreny <38407983+lorreny@users.noreply.github.com>
2024-06-08 10:01:44 -0300

    Update lista_exercicio_4.js

--- modulo-4/lista_exercicio_4.js
+++ modulo-4/lista_exercicio_4.js
@@ -7,3 +7,43 @@ let sobrenomes = ['Oliveira', 'Santos', 'Marques', 'Castro', 'Silva']
 for (let i = 0; i < nomes.length; i++){
     console.log(nomes[i], sobrenomes[i])
 }
+
+
+// 4 - Observe as seguintes listas:
+/* 
+let nomes = ['Bruna', 'Gabriel', 'Marcos', 'Jéssica', 'Fernando']
+let sobrenomes = ['Oliveira', 'Santos', 'Marques', 'Castro']
+
+//Crie um algirtmo que combine o valor do nome com o sobrenome formando o nome completo.
+// O programa deve iterar sobre as listas fornecidas e juntar os valores correspondentes.
+/*for (let i = 0; i < nomes.length; i++){
+    console.log(nomes[i], sobrenomes[i])
+}*/
+
+/*
+for (let i = 0; i < nomes.length; i++){
+    for (let i = 0; i < sobrenomes.length; i++){
+    }
+    console.log(nomes[i], sobrenomes[i])
+
+}*/
+
+const nomes = ['Bruna', 'Gabriel', 'Marcos', 'Jéssica', 'Fernando']
+const sobrenomes = ['Oliveira', 'Santos', 'Marques']
+const tamanhoMinimo = Math.min(nomes.length, sobrenomes.length)
+const listaNomesCompletos = []
+for(let i = 0; i < tamanhoMinimo; i++){
+    const nome = nomes[i].trim()
+    const sobrenome = sobrenomes[i].trim()
+    listaNomesCompletos.push(nome + ' ' + sobrenome)
+}
+
+let itensRestantes = []
+if (nomes.length > tamanhoMinimo){
+    itensRestantes.push(...nomes.slice(tamanhoMinimo))
+}
+if (sobrenomes.length > tamanhoMinimo){
+    itensRestantes.push(...sobrenomes.slice(tamanhoMinimo))
+}
+console.log(listaNomesCompletos)
+console.log(itensRestantes)
