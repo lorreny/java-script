96dd6cd43456591bd953e59cea4272f6b0dbd892
Lorreny <38407983+lorreny@users.noreply.github.com>
2024-06-08 08:52:15 -0300

    Create lista_exercicio_2.js

---
+++ modulo-4/lista_exercicio_2.js
@@ -0,0 +1,8 @@
+// 2 - Crie um algoritmo que lê uma palavra e imprime as letras separadamente e cada letra em caixa alta (maiúscula)
+let palavra = 'Abóbora'
+
+
+for (let i = 0; i < palavra.length; i++){
+    console.log(palavra[i].toUpperCase())
+
+}
