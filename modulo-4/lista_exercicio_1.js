fea8372c4166ac6387ce4864cf3e31022e1e41aa
Lorreny <38407983+lorreny@users.noreply.github.com>
2024-06-08 08:51:55 -0300

    Create lista_exercicio_1.js

---
+++ modulo-4/lista_exercicio_1.js
@@ -0,0 +1,7 @@
+// 1 - Crie um algoritmo que imprima todos os números de até um valor alvo.
+// Ex: Se o valor alvo é 10, o programa deve imprimir: 1, 2, 3, 4, 5, 6, 7, 8, 9 e 10;
+let numeros = 10
+
+for (let i = 1; i <= numeros; i++){
+    console.log(i)
+}
