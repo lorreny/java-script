c4b5d52d22a1a809de7f2547b22f8b1461f2d2ce
Lorreny <38407983+lorreny@users.noreply.github.com>
2024-06-08 10:23:59 -0300

    Create lista_exercicio_4._1prf.js

---
+++ modulo-4/lista_exercicio_4._1prf.js
@@ -0,0 +1,9 @@
+//prof
+let nomes = ['Bruna', 'Gabriel', 'Marcos', 'Jéssica', 'Fernando']
+let sobrenomes = ['Oliveira', 'Santos', 'Marques', 'Castro', 'Silva']
+
+for (let i = 0; i < nomes.length; i++){
+    if(nomes[i] && sobrenomes[i]){
+    console.log(nomes[i], sobrenomes[i])
+    }
+}
