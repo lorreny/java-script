1e59d76e49d1bb464842f629a7ca1f1165ff1fde
Lorreny <38407983+lorreny@users.noreply.github.com>
2024-06-15 08:02:34 -0300

    Update lista_exercicio_7.js

--- modulo-4/lista_exercicio_7.js
+++ modulo-4/lista_exercicio_7.js
@@ -5,3 +5,31 @@ let lista = ['Mariana', 'Luiz', 'João', 'Amanda']
 for (let i = 0; i < lista.length; i++){
     console.log(lista[i].split('').reverse().join(''))
 }
+
+//prof
+// 7 - Crie um algoritmo que lê uma lista de palavras e inverte as palavras, sem alterar a ordem da lista original. 
+// Ex: lista = ['Mariana', 'Luiz', 'João', 'Amanda']
+// output esperado: ['Anairam', 'Ziul', 'Oãoj', 'Adnama']
+
+let lista = ['Mariana', 'Luiz', 'João', 'Amanda']
+
+// Sei que vou precisar iterar sobre a lista de nomes.
+
+let nova_lista = []
+
+for (let nome of lista) {
+    // escrever o nome ao contrário
+    // Strings se comportam como listas.
+    let palavra = ''   //Oãoj
+    for (let i = nome.length - 1; i >= 0; i-=1) {
+        // console.log(nome[i])
+        if (palavra === '') {
+            palavra = palavra + nome[i].toUpperCase()   //O
+        } else {
+            palavra = palavra + nome[i].toLowerCase()  //ãoj
+        }
+    }
+    nova_lista.push(palavra)
+}
+
+console.log(nova_lista)
