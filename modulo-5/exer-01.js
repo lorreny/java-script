144dbbec97c9aba975a44acbfaa4bba11d7626da
Lorreny <38407983+lorreny@users.noreply.github.com>
2024-06-15 09:37:45 -0300

    Create exer-01.js

---
+++ modulo-5/exer-01.js
@@ -0,0 +1,10 @@
+// 1 - Declare uma variável do tipo lista e imprima seus valores separadamente no terminal.
+
+let names = ["Alice", "Bob", "Charlie", "Diana", "Ethan", "Fiona", "George", "Hannah", "Ian", "Julia"];
+
+//console.log(names)
+
+for (let i = 0; i < names.length; i += 1) {
+  console.log('Valores separados: ', names[i])
+}
+
