bf400c4c296c2b6fc922512dd628499ec6547be2
Lorreny <38407983+lorreny@users.noreply.github.com>
2024-06-15 09:42:36 -0300

    Create exer-06.js

---
+++ modulo-5/exer-06.js
@@ -0,0 +1,10 @@
+// 6 - Remova um valor de uma posição específica  da lista e adicione dois novos valores.
+
+let names = ["Alice", "Bob", "Charlie", "Diana", "Ethan", "Fiona", "George", "Hannah", "Ian", "Julia"];
+
+console.log('Lista antes da remoção', names, 'Adicionando valores:', )
+names.splice(2, 5, 'Ariane', 'Daniel')
+console.log('Removendo valores e adicionando', names)
+
+
+
