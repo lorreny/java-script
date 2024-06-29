64ebc3fb35514144c41ec54414254b7e912298d4
Lorreny <38407983+lorreny@users.noreply.github.com>
2024-06-29 09:53:48 -0300

    Create exercicio-1-2-3.js

---
+++ modulo-6/exercicio-1-2-3.js
@@ -0,0 +1,17 @@
+// Para melhor organização recomenda-se criarem arquivos separados para cada exercícios;
+// A nomeação padrão dos arquivos é: "Ex01" para o primeiro e assim por diante;
+
+// 1 - Crie um programa que exibe a mensagem "Olá, Bruno!"
+
+// 2 - Crie um programa que exiba a mensagem "Olá, Amanda!"
+
+// 3 - Crie um programa que exiba a mesma mensagem porém com o seu nome.
+
+function nomes(nome){
+    return nome
+}
+result = "Olá,"
+
+console.log(result, 'Bruno')
+console.log(result, 'Amanda')
+console.log(result, 'Shellen')
